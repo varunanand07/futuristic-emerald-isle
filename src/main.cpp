9ce2f4af726e32a5ed555ae0196f5e79501bb2a0
varunanand07 <anandvarun2003@gmail.com>
2024-12-27 04:50:04 +0000

    Update main.cpp to apply textures correctly
    - Ensured container.jpg is applied exclusively to the container (cube).
    - Ensured ground_texture.jpg is applied solely to the ground plane.
    - Updated code to load both textures separately and bind them appropriately during rendering.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "shader.h"
@@ -9,25 +10,20 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-
 const GLuint WIDTH = 800, HEIGHT = 600;
-
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
-
 Camera camera;
 float lastX = WIDTH / 2.0f;
 float lastY = HEIGHT / 2.0f;
 bool firstMouse = true;
 
-
-float deltaTime = 0.0f;	
+float deltaTime = 0.0f;
 float lastFrame = 0.0f;
-
 
 void glfw_error_callback(int error, const char* description)
 {
@@ -36,26 +32,22 @@ void glfw_error_callback(int error, const char* description)
 
 int main()
 {
-    
     glfwSetErrorCallback(glfw_error_callback);
 
-    
     if (!glfwInit())
     {
         std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Toward a Futuristic Emerald Isle", NULL, NULL);
     if (!window)
     {
@@ -64,130 +56,106 @@ int main()
         return -1;
     }
 
-    
     glfwMakeContextCurrent(window);
 
-    
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    
-    glewExperimental = GL_TRUE; 
+    glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK)
     {
         std::cerr << "Failed to initialize GLEW\n";
         return -1;
     }
 
-    
     glViewport(0, 0, WIDTH, HEIGHT);
     std::cout << "Viewport set to " << WIDTH << "x" << HEIGHT << ".\n";
 
-    
     glEnable(GL_DEPTH_TEST);
     std::cout << "Depth testing enabled.\n";
 
-    
     Shader ourShader("../shaders/vertex_shader.glsl", "../shaders/fragment_shader.glsl");
     std::cout << "Shaders compiled and linked.\n";
 
-    
     GLfloat vertices[] = {
-        
-        
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f, 
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 0.0f, 
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f, 
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f, 
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 1.0f, 
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f, 
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
 
-        
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f, 
-         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f, 
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f, 
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f, 
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f, 
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f, 
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
 
-        
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f, 
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f, 
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f, 
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
 
-        
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f, 
-         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f, 
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f, 
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
 
-        
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f, 
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f, 
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f, 
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f, 
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f, 
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f, 
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
 
-        
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f, 
-         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f, 
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f, 
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f, 
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f, 
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f  
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f
     };
 
     GLuint VBO, VAO;
-    glGenVertexArrays(1, &VAO); 
-    glGenBuffers(1, &VBO);      
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-    
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
-    
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
-    
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
 
-    glBindVertexArray(0); 
+    glBindVertexArray(0);
 
     std::cout << "VAO and VBO set up.\n";
 
-    
     GLuint texture1;
     glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1); 
+    glBindTexture(GL_TEXTURE_2D, texture1);
 
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    
     int width, height, nrChannels;
-    
     stbi_set_flip_vertically_on_load(true);
     unsigned char *data = stbi_load("../textures/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
@@ -206,15 +174,45 @@ int main()
     }
     else
     {
-        std::cerr << "Failed to load texture\n";
+        std::cerr << "Failed to load container texture\n";
     }
     stbi_image_free(data);
 
-    
-    ourShader.use(); 
+    GLuint groundTexture;
+    glGenTextures(1, &groundTexture);
+    glBindTexture(GL_TEXTURE_2D, groundTexture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int groundWidth, groundHeight, groundNrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *groundData = stbi_load("../textures/ground_texture.jpg", &groundWidth, &groundHeight, &groundNrChannels, 0);
+    if (groundData)
+    {
+        GLenum format;
+        if (groundNrChannels == 1)
+            format = GL_RED;
+        else if (groundNrChannels == 3)
+            format = GL_RGB;
+        else if (groundNrChannels == 4)
+            format = GL_RGBA;
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, groundWidth, groundHeight, 0, format, GL_UNSIGNED_BYTE, groundData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        std::cout << "Texture loaded successfully: ground_texture.jpg\n";
+    }
+    else
+    {
+        std::cerr << "Failed to load ground texture\n";
+    }
+    stbi_image_free(groundData);
+
+    ourShader.use();
     glUniform1i(glGetUniformLocation(ourShader.Program, "texture1"), 0);
 
-    
     GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
     GLint viewLoc  = glGetUniformLocation(ourShader.Program, "view");
     GLint projLoc  = glGetUniformLocation(ourShader.Program, "projection");
@@ -225,7 +223,6 @@ int main()
     GLint fogColorLoc = glGetUniformLocation(ourShader.Program, "fogColor");
     GLint fogDensityLoc = glGetUniformLocation(ourShader.Program, "fogDensity");
 
-    
     if(modelLoc == -1 || viewLoc == -1 || projLoc == -1 ||
        lightPosLoc == -1 || lightColorLoc == -1 ||
        viewPosLoc == -1 || objectColorLoc == -1 ||
@@ -234,17 +231,13 @@ int main()
         std::cerr << "Error: One or more uniform locations not found.\n";
     }
 
-    
-    glm::vec3 lightPos(2.0f, 2.0f, 2.0f); 
-    glm::vec3 lightColor(1.0f, 1.0f, 1.0f); 
-    glm::vec3 objectColor(1.0f, 1.0f, 1.0f); 
-    glm::vec3 fogColor(0.05f, 0.2f, 0.1f); 
+    glm::vec3 lightPos(2.0f, 2.0f, 2.0f);
+    glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+    glm::vec3 objectColor(1.0f, 1.0f, 1.0f);
+    glm::vec3 fogColor(0.05f, 0.2f, 0.1f);
     float fogDensity = 0.05f;
 
-    
-    
     GLfloat lightVertices[] = {
-        
         -0.05f, -0.05f, -0.05f,
          0.05f, -0.05f, -0.05f,
          0.05f,  0.05f, -0.05f,
@@ -288,35 +281,27 @@ int main()
         -0.05f,  0.05f, -0.05f
     };
 
-    
     GLuint lightVBO, lightVAO;
     glGenVertexArrays(1, &lightVAO);
     glGenBuffers(1, &lightVBO);
 
-    
     glBindVertexArray(lightVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(lightVertices), lightVertices, GL_STATIC_DRAW);
 
-    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
-    glBindVertexArray(0); 
+    glBindVertexArray(0);
 
     std::cout << "Light VAO and VBO set up.\n";
 
-    
     Shader lightShader("../shaders/light_vertex_shader.glsl", "../shaders/light_fragment_shader.glsl");
     lightShader.use();
     glUniform3f(glGetUniformLocation(lightShader.Program, "lightColor"), lightColor.x, lightColor.y, lightColor.z);
 
-    
-    
     GLfloat groundVertices[] = {
-        
-        
         -5.0f, -0.5f, -5.0f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,
          5.0f, -0.5f, -5.0f,  0.0f, 1.0f, 0.0f,  5.0f, 0.0f,
          5.0f, -0.5f,  5.0f,  0.0f, 1.0f, 0.0f,  5.0f, 5.0f,
@@ -326,7 +311,6 @@ int main()
         -5.0f, -0.5f, -5.0f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f
     };
 
-    
     GLuint groundVBO, groundVAO;
     glGenVertexArrays(1, &groundVAO);
     glGenBuffers(1, &groundVBO);
@@ -336,97 +320,43 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(groundVertices), groundVertices, GL_STATIC_DRAW);
 
-    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
-    
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
-    
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
 
-    glBindVertexArray(0); 
+    glBindVertexArray(0);
 
     std::cout << "Ground VAO and VBO set up.\n";
 
-    
-    GLuint groundTexture;
-    glGenTextures(1, &groundTexture);
-    glBindTexture(GL_TEXTURE_2D, groundTexture); 
 
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    
-    int groundWidth, groundHeight, groundNrChannels;
-    
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char *groundData = stbi_load("../textures/ground_texture.jpg", &groundWidth, &groundHeight, &groundNrChannels, 0);
-    if (groundData)
-    {
-        GLenum format;
-        if (groundNrChannels == 1)
-            format = GL_RED;
-        else if (groundNrChannels == 3)
-            format = GL_RGB;
-        else if (groundNrChannels == 4)
-            format = GL_RGBA;
-
-        glTexImage2D(GL_TEXTURE_2D, 0, format, groundWidth, groundHeight, 0, format, GL_UNSIGNED_BYTE, groundData);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        std::cout << "Ground texture loaded successfully: ground_texture.jpg\n";
-    }
-    else
-    {
-        std::cerr << "Failed to load ground texture\n";
-    }
-    stbi_image_free(groundData);
-
-    
-    
-    
-
-    
     ourShader.use();
     glUniform3fv(objectColorLoc, 1, glm::value_ptr(objectColor));
     glUniform3fv(fogColorLoc, 1, glm::value_ptr(fogColor));
     glUniform1f(fogDensityLoc, fogDensity);
 
-    
-    
-
     std::cout << "Entering main loop.\n";
 
-    
     while (!glfwWindowShouldClose(window))
     {
-        
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        
         processInput(window);
 
-        
         lightPos.x = sin(glfwGetTime()) * 2.0f;
         lightPos.z = cos(glfwGetTime()) * 2.0f;
 
-        
-        glClearColor(0.05f, 0.2f, 0.1f, 1.0f); 
+        glClearColor(0.05f, 0.2f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        
         ourShader.use();
 
-        
         glm::mat4 model = glm::rotate(glm::mat4(1.0f), 
             (GLfloat)glfwGetTime() * glm::radians(50.0f), 
             glm::vec3(0.5f, 1.0f, 0.0f));
@@ -434,42 +364,34 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 
             (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
 
-        
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        
         glUniform3fv(lightPosLoc, 1, glm::value_ptr(lightPos));
         glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
         glUniform3fv(viewPosLoc, 1, glm::value_ptr(camera.Position));
 
-        
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
-        
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
 
-        
+        glBindTexture(GL_TEXTURE_2D, groundTexture);
+
         glm::mat4 groundModel = glm::mat4(1.0f);
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(groundModel));
-
-        
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1); 
 
         glBindVertexArray(groundVAO);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
 
-        
         lightShader.use();
         glm::mat4 lightModel = glm::mat4(1.0f);
         lightModel = glm::translate(lightModel, lightPos);
-        lightModel = glm::scale(lightModel, glm::vec3(1.0f)); 
+        lightModel = glm::scale(lightModel, glm::vec3(1.0f));
         glUniformMatrix4fv(glGetUniformLocation(lightShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
         glUniformMatrix4fv(glGetUniformLocation(lightShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(lightShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
@@ -478,28 +400,24 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
 
-        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     std::cout << "Exiting main loop.\n";
 
-    
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteTextures(1, &texture1);
-    glDeleteVertexArrays(1, &lightVAO);
-    glDeleteBuffers(1, &lightVBO);
     glDeleteVertexArrays(1, &groundVAO);
     glDeleteBuffers(1, &groundVBO);
-    glDeleteTextures(1, &groundTexture); 
+    glDeleteTextures(1, &groundTexture);
+    glDeleteVertexArrays(1, &lightVAO);
+    glDeleteBuffers(1, &lightVBO);
 
-    
     glfwTerminate();
     return 0;
 }
-
 
 void processInput(GLFWwindow *window)
 {
@@ -516,18 +434,14 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     std::cout << "Framebuffer resized to " << width << "x" << height << "\n";
     glViewport(0, 0, width, height);
 }
 
-
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-    
-    
 
     if(firstMouse)
     {
@@ -537,7 +451,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     }
 
     float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; 
+    float yoffset = lastY - ypos;
 
     lastX = xpos;
     lastY = ypos;
@@ -545,10 +459,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
-
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    
-    
     camera.ProcessMouseScroll(yoffset);
 }
