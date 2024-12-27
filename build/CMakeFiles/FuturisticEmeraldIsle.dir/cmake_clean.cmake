2af6d5c619bc0cf2201ffbdef1846113f4a35e5b
varunanand07 <anandvarun2003@gmail.com>
2024-12-27 03:54:03 +0000

    Rendered a basic triangle

---
+++ build/CMakeFiles/FuturisticEmeraldIsle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FuturisticEmeraldIsle.dir/src/main.cpp.o"
+  "CMakeFiles/FuturisticEmeraldIsle.dir/src/main.cpp.o.d"
+  "CMakeFiles/FuturisticEmeraldIsle.dir/src/shader.cpp.o"
+  "CMakeFiles/FuturisticEmeraldIsle.dir/src/shader.cpp.o.d"
+  "FuturisticEmeraldIsle"
+  "FuturisticEmeraldIsle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FuturisticEmeraldIsle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
